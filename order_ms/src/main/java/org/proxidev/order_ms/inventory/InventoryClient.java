a7264801b269972af14bc5ed83275b7685226045
joelproxi <joeledmond95@gmail.com>
2024-10-23 12:51:33 +0200

    refactory

--- order_ms/src/main/java/org/proxidev/order_ms/inventory/InventoryClient.java
+++ order_ms/src/main/java/org/proxidev/order_ms/inventory/InventoryClient.java
@@ -3,12 +3,12 @@ package org.proxidev.order_ms.inventory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(url = "http://localhost:9501/api/v1/inventories", name = "inventory-service")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
 
-    @GetMapping("/{productId}")
+    @GetMapping("/api/v1/inventories/{productId}")
     InventoryDTO getInventoryById(@PathVariable Long productId);
 
-    @PutMapping("/{productId}")
+    @PutMapping("/api/v1/inventories/{productId}")
     InventoryDTO updateQuantity(@PathVariable Long productId, @RequestBody Integer quantity);
 }
--- product-service/src/main/java/org/proxidev/productservice/inventory/InventoryService.java
+++ product-service/src/main/java/org/proxidev/productservice/inventory/InventoryService.java
@@ -1,21 +1,22 @@
 package org.proxidev.productservice.inventory;
 
+import lombok.RequiredArgsConstructor;
 import org.proxidev.productservice.product.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class InventoryService {
-
+    private final RestTemplate restTemplate;
     public boolean createInventory(Product product) {
         InventoryDTO inventoryDTO = InventoryDTO.builder()
                 .productId(product.getId())
                 .quantity(product.getQuantity())
                 .build();
-        RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Void> resp = restTemplate.postForEntity(
-                "http://localhost:9501"+ "/api/v1/inventories",
+                "http://inventory-service"+ "/api/v1/inventories",
                 inventoryDTO, Void.class);
         return resp.getStatusCode().is2xxSuccessful();
     }
